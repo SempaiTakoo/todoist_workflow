a1322e268b3866d2619db24ae68e08c5e3fb3151
SempaiTakoo <idl76h@gmail.com>
2025-04-27 15:39:07 +0300

    Add move tasks by rule

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ TOKEN = os.getenv('TOKEN')
 SYNC_URL = 'https://api.todoist.com/sync/v9/sync'
 
 
+# def create_projects_by_labels(query: str, labels: list[str]) -> None:
+#     repo = TodoistRepository(TOKEN)
+#     tasks: list[Task] = repo.filter_tasks(query=query)
+
+#     tasks_with_subtasks: set[str] = set(
+#         t.parent_id for t in tasks if t.parent_id is not None
+#     )
+
+#     move_tasks_by_rule()
+
+
+
 def move_tasks_by_rule(query: str, rule: dict[str, str]) -> None:
     repo = TodoistRepository(TOKEN)
     tasks: list[Task] = repo.filter_tasks(query=query)
@@ -18,11 +30,17 @@ def move_tasks_by_rule(query: str, rule: dict[str, str]) -> None:
     for t in tasks:
         if not t.labels:
             continue
-        label = filter(lambda label: label in t.labels, rule)
-        sync_repo.add_move_item_command(
-            task_id=t.id,
-            project_id=rule[label]
-        )
+
+        destination = ''
+        for label in rule:
+            if label in t.labels:
+                destination = rule[label]
+
+        if destination:
+            sync_repo.add_move_item_command(
+                task_id=t.id,
+                project_id=destination
+            )
     sync_repo.send_commands()
 
 
@@ -39,3 +57,4 @@ if __name__ == "__main__":
         "#потом/неясно": "6XmVVx4ChwxWffFx",
     }
     move_tasks_by_rule(query="#Inbox", rule=labels_to_project_ids)
+    # create_projects_by_labels(query="#Inbox", labels=[])
